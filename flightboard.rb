a96c2754066e702d82b3fe275671dd312ccbe122
tdawe <trevor.dawe@gmail.com>
2012-05-16 20:57:05 -0300

    Start using vatsim gem with basic sample

--- flightboard.rb
+++ flightboard.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
+require 'vatsim'
 
 get '/' do
-  "<h1>flightboard</h1>coming soon"
+  "<h1>#{Vatsim::Data.new.pilots.length} pilots</h1><h1>#{Vatsim::Data.new.atc.length} atc</h1>"
 end
+
