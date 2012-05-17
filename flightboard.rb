b5c353887f2952ab465e7d39776c8843c8dc7bf2
tdawe <trevor.dawe@gmail.com>
2012-05-16 21:17:40 -0300

    Added /pilots and /pilots/:callsign paths

--- flightboard.rb
+++ flightboard.rb
@@ -2,6 +2,40 @@ require 'sinatra'
 require 'vatsim'
 
 get '/' do
-  "<h1>#{Vatsim::Data.new.pilots.length} pilots</h1><h1>#{Vatsim::Data.new.atc.length} atc</h1>"
+  data = Vatsim::Data.new
+
+  "<h1><a href=\"/pilots\">#{data.pilots.length} pilots</a></h1><h1>#{data.atc.length} atc</h1>Update: #{data.general['update']}"
+end
+
+get '/pilots' do
+  pilots = Vatsim::Data.new.pilots
+  output = ""
+
+  pilots.each { |pilot|
+    output += "<a href=\"/pilots/#{pilot.callsign}\">#{pilot.callsign}<br/>"
+  }
+
+  output
+
+end
+
+get '/pilots/:callsign' do
+  pilots = Vatsim::Data.new.pilots
+  _pilot = nil
+
+  pilots.each { |pilot|
+    _pilot = pilot if pilot.callsign.eql? params[:callsign]
+  }
+
+  output = "<h1>#{_pilot.callsign}</h1>"
+
+  output += "<p>#{_pilot.planned_depairport} - #{_pilot.planned_destairport}</p>"
+
+  output += "<p>Route: #{_pilot.planned_route}</p>"
+
+  output += "<p>Aircraft: #{_pilot.planned_aircraft}</p>"
+
+  output
+
 end
 
