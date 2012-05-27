d6d990cde436f0b64d515e6c236b370d3d29f258
tdawe <trevor.dawe@gmail.com>
2012-05-27 20:33:27 -0300

    Store data in database instead of always retrieving from vatsim gem

--- Gemfile
+++ Gemfile
@@ -1,3 +1,19 @@
 source 'http://rubygems.org'
 gem 'sinatra'
 gem 'vatsim'
+gem 'datamapper'
+
+group :development do
+  gem 'sqlite3-ruby'
+  gem 'dm-sqlite-adapter'
+end
+
+group :test do
+  gem 'rack-test'
+  gem 'rspec'
+end
+
+group :production do
+  gem 'pg'
+  gem 'dm-postgres-adapter'
+end
--- flightboard.rb
+++ flightboard.rb
@@ -1,17 +1,71 @@
 require 'sinatra'
 require 'vatsim'
+require 'data_mapper'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.db")
+
+class Pilot
+  include DataMapper::Resource
+  property :callsign, String, :key => true
+  property :cid, String 
+  property :realname, String 
+  property :clienttype, String 
+  property :latitude, String 
+  property :longitude, String 
+  property :altitude, String 
+  property :groundspeed, String 
+  property :planned_aircraft, String 
+  property :planned_tascruise, String 
+  property :planned_depairport, String 
+  property :planned_altitude, String 
+  property :planned_destairport, String 
+  property :server, String 
+  property :protrevision, String 
+  property :rating, String 
+  property :transponder, String 
+  property :planned_revision, String 
+  property :planned_flighttype, String 
+  property :planned_deptime, String
+  property :planned_actdeptime, String
+  property :planned_hrsenroute, String 
+  property :planned_minenroute, String 
+  property :planned_hrsfuel, String 
+  property :planned_minfuel, String 
+  property :planned_altairport, String 
+  property :planned_remarks, String 
+  property :planned_route, String 
+  property :planned_depairport_lat, String 
+  property :planned_depairport_lon, String 
+  property :planned_destairport_lat, String 
+  property :planned_destairport_lon, String 
+  property :time_logon, String 
+  property :heading, String 
+  property :QNH_iHg, String 
+  property :QNH_Mb, String
+end
+DataMapper.finalize
+DataMapper.auto_migrate!
+
+get '/refreshdata' do
+  output = ""
+  data = Vatsim::Data.new
+  Pilot.transaction do
+    Pilot.destroy
+    data.pilots.each { |pilot|
+      Pilot.create(:callsign => pilot.callsign, :cid => pilot.cid, :realname => pilot.realname, :clienttype => pilot.clienttype, :latitude => 	     pilot.latitude, :longitude => pilot.longitude, :altitude => pilot.altitude, :groundspeed => pilot.groundspeed, :planned_aircraft =>           pilot.planned_aircraft, :planned_tascruise => pilot.planned_tascruise, :planned_depairport => pilot.planned_depairport, :planned_altitude => pilot.planned_altitude, :planned_destairport => pilot.planned_destairport, :server => pilot.server, :protrevision => pilot.protrevision, :rating => pilot.rating, :transponder => pilot.transponder, :planned_revision => pilot.planned_revision, :planned_flighttype => pilot.planned_flighttype, :planned_deptime => pilot.planned_deptime, :planned_actdeptime => pilot.planned_actdeptime, :planned_hrsenroute => pilot.planned_minenroute, :planned_hrsfuel => pilot.planned_minfuel, :planned_altairport => pilot.planned_altairport, :planned_remarks => pilot.planned_remarks, :planned_route => pilot.planned_route, :planned_depairport_lat => pilot.planned_depairport_lat, :planned_depairport_lon => pilot.planned_depairport_lon, :planned_destairport_lat => pilot.planned_destairport_lat, :planned_destairport_lon => pilot.planned_destairport_lon, :time_logon => pilot.time_logon, :heading => pilot.heading, :QNH_iHg => pilot.QNH_iHg, :QNH_Mb => pilot.QNH_Mb)
+    }
+  end
+  "Loaded #{Pilot.all.length} pilots"
+end
 
 get '/' do
-  data = Vatsim::Data.new
-
-  "<h1><a href=\"/pilots\">#{data.pilots.length} pilots</a></h1><h1>#{data.atc.length} atc</h1>Update: #{data.general['update']}"
+  "<h1><a href=\"/pilots\">#{Pilot.all.length} pilots</a></h1>"
 end
 
 get '/pilots' do
-  pilots = Vatsim::Data.new.pilots
   output = ""
 
-  pilots.each { |pilot|
+  Pilot.all.each { |pilot|
     output += "<a href=\"/pilots/#{pilot.callsign}\">#{pilot.callsign}<br/>"
   }
 
@@ -20,20 +74,11 @@ get '/pilots' do
 end
 
 get '/pilots/:callsign' do
-  pilots = Vatsim::Data.new.pilots
-  _pilot = nil
+  pilot = Pilot.get(params[:callsign])
 
-  pilots.each { |pilot|
-    _pilot = pilot if pilot.callsign.eql? params[:callsign]
-  }
+  output = "<h1>#{pilot.callsign}</h1>"
 
-  output = "<h1>#{_pilot.callsign}</h1>"
-
-  output += "<p>#{_pilot.planned_depairport} - #{_pilot.planned_destairport}</p>"
-
-  output += "<p>Route: #{_pilot.planned_route}</p>"
-
-  output += "<p>Aircraft: #{_pilot.planned_aircraft}</p>"
+  output += "<p>#{pilot.planned_depairport} - #{pilot.planned_destairport}</p>"
 
   output
 
