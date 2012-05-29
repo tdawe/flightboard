38d72a94d0bd718b78eb5f013d817f83e232926d
tdawe <trevor.dawe@gmail.com>
2012-05-28 22:09:05 -0300

    Added haml templates for displaying airports and simple arrivals/departures

--- flightboard.rb
+++ flightboard.rb
@@ -42,3 +42,19 @@ get '/pilots/:callsign' do
   haml :pilot
 end
 
+get '/airports' do
+  pilots = Pilot.all
+  @airports = Array.new
+  pilots.each { |pilot|
+    @airports << pilot.planned_depairport if !@airports.include?(pilot.planned_depairport)
+    @airports << pilot.planned_destairport if !@airports.include?(pilot.planned_destairport)
+  }
+  haml :airports
+end
+
+get '/airports/:icao' do
+  @arrivals = Pilot.all(:planned_destairport => params[:icao])
+  @departures = Pilot.all(:planned_depairport => params[:icao])
+  haml :airport
+end
+
