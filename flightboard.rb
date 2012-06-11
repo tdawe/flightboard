e391bee0d30c57340adda5479088f490d80aa60e
tdawe <trevor.dawe@gmail.com>
2012-06-11 20:21:53 -0300

    Use the General 'update' parameter as Time.now when determining departure and arrival dates

--- flightboard.rb
+++ flightboard.rb
@@ -14,13 +14,14 @@ get '/refreshdata' do
   output = ""
   data = Vatsim::Data.new
   Pilot.transaction do
+    now = Time.parse(data.general['update'])
     Pilot.destroy
     data.pilots.each { |pilot|
-      _scheduled_departure_time = scheduled_departure_time(pilot)
-      _scheduled_arrival_time = scheduled_arrival_time(pilot)
-      _estimated_arrival_time = estimated_arrival_time(pilot)
-      _arrival_status = arrival_status(pilot)
-      _flight_status = flight_status(pilot)
+      _scheduled_departure_time = scheduled_departure_time now, pilot
+      _scheduled_arrival_time = scheduled_arrival_time now, pilot
+      _estimated_arrival_time = estimated_arrival_time now, pilot
+      _arrival_status = arrival_status now, pilot
+      _flight_status = flight_status pilot
       Pilot.create(:callsign => pilot.callsign, :cid => pilot.cid, :realname => pilot.realname, :clienttype => pilot.clienttype, :latitude => pilot.latitude, :longitude => pilot.longitude, :altitude => pilot.altitude, :groundspeed => pilot.groundspeed, :planned_aircraft =>           pilot.planned_aircraft, :planned_tascruise => pilot.planned_tascruise, :planned_depairport => pilot.planned_depairport, :planned_altitude => pilot.planned_altitude, :planned_destairport => pilot.planned_destairport, :server => pilot.server, :protrevision => pilot.protrevision, :rating => pilot.rating, :transponder => pilot.transponder, :planned_revision => pilot.planned_revision, :planned_flighttype => pilot.planned_flighttype, :planned_deptime => pilot.planned_deptime, :planned_actdeptime => pilot.planned_actdeptime, :planned_hrsenroute => pilot.planned_minenroute, :planned_hrsfuel => pilot.planned_minfuel, :planned_altairport => pilot.planned_altairport, :planned_remarks => pilot.planned_remarks, :planned_route => pilot.planned_route, :planned_depairport_lat => pilot.planned_depairport_lat, :planned_depairport_lon => pilot.planned_depairport_lon, :planned_destairport_lat => pilot.planned_destairport_lat, :planned_destairport_lon => pilot.planned_destairport_lon, :time_logon => pilot.time_logon, :heading => pilot.heading, :QNH_iHg => pilot.QNH_iHg, :QNH_Mb => pilot.QNH_Mb, :scheduled_departure_time => _scheduled_departure_time, :scheduled_arrival_time => _scheduled_arrival_time, :estimated_arrival_time => _estimated_arrival_time, :arrival_status => _arrival_status, :flight_status => _flight_status)
     }
   end
@@ -65,8 +66,7 @@ end
 
 ##############################333
 
-def scheduled_departure_time pilot
-  now = Time.now
+def scheduled_departure_time now, pilot
   departure_time = pilot.planned_deptime.eql?("0") ? nil : pilot.planned_deptime
   #handles invalid times
   departure_time = nil if pilot.planned_deptime.to_i > 2359
@@ -80,20 +80,20 @@ def scheduled_departure_time pilot
   return departure_time
 end
 
-def scheduled_arrival_time pilot
-  deptime = scheduled_departure_time pilot
+def scheduled_arrival_time now, pilot
+  deptime = scheduled_departure_time now, pilot
   return deptime if deptime.nil?
   return deptime + (pilot.planned_hrsenroute.to_i * 60*60) + (pilot.planned_minenroute.to_i*60)
 end
 
-def estimated_arrival_time pilot
+def estimated_arrival_time now, pilot
   _flight_status = flight_status(pilot)
   return nil if(!(_flight_status.eql?("On the way") or _flight_status.eql?("Arriving"))) # or !_flight_status.eql?("Arriving"))
   return nil if pilot.planned_destairport_lat.eql?("0")
   distance = haversine_distance(pilot.latitude.to_f, pilot.longitude.to_f, pilot.planned_destairport_lat.to_f, pilot.planned_destairport_lon.to_f)
   return nil if pilot.groundspeed.to_i == 0
   time_for_distance = distance / pilot.groundspeed.to_i
-  return Time.now.getgm + (time_for_distance * 60 * 60)
+  return now.getgm + (time_for_distance * 60 * 60)
 end
 
 def flight_status pilot
@@ -109,14 +109,14 @@ def flight_status pilot
   status
 end
 
-def arrival_status pilot
+def arrival_status now, pilot
   status = "On Time"
 
   distance_from_depairport = haversine_distance(pilot.latitude.to_f, pilot.longitude.to_f, pilot.planned_depairport_lat.to_f, pilot.planned_depairport_lon.to_f)
 
-  return "N/A" if scheduled_arrival_time(pilot).nil? or estimated_arrival_time(pilot).nil?
-  status = "Late" if estimated_arrival_time(pilot) > scheduled_arrival_time(pilot)
-  status = "Early" if estimated_arrival_time(pilot) < scheduled_arrival_time(pilot)
+  return "N/A" if scheduled_arrival_time(now, pilot).nil? or estimated_arrival_time(now, pilot).nil?
+  status = "Late" if estimated_arrival_time(now, pilot) > scheduled_arrival_time(now, pilot)
+  status = "Early" if estimated_arrival_time(now, pilot) < scheduled_arrival_time(now, pilot)
   return status
 end
 
